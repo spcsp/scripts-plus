7b772a9b9499296d51ab975189ebb488ccca983e
Kevin Hill <kevinkhill@gmail.com>
2021-02-18 08:20:22 -0800

    drag and drop xxx.sp.js files

---
+++ tools/sp/strokesplus.js
@@ -0,0 +1,16 @@
+const execa = require("execa");
+
+const EXE_PATH = String.raw`"C:\Program Files\StrokesPlus.net\StrokesPlus.net.exe"`;
+
+const sp = async (src) => execa(EXE_PATH, [`--script=${src}`]);
+
+sp.eval = s => sp(`eval(${s});`);
+
+sp.readFile = s => sp.eval(`File.ReadAllText(${s})`);
+
+sp.exec = absPath => sp.readFile("String.raw`" + absPath + "`");
+
+module.exports = {
+  sp,
+  EXE_PATH
+};
