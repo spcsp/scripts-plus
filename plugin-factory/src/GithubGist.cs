fdd49995654acb9e9a402872ae98404653c0cf25
Kevin Hill <kevinkhill@gmail.com>
2021-05-09 08:00:45 -0700

    missing async and name issue

--- plugin-factory/src/GithubGist.cs
+++ plugin-factory/src/GithubGist.cs
@@ -1,17 +1,17 @@
 using Microsoft.ClearScript.V8;
 
-namespace EvalFilePlugin
+namespace GithubGistPlugin
 {
-  public static class EvalFilePlugin
+  public static class GithubGistPlugin
   {
     public static void StrokesPlusInitStaticPlugin(V8ScriptEngine e)
     {
       string[] lines = {
-        "function GithubGist(user, hash, evalResult = true) {",
+        "async function GithubGist(user, hash, evalResult = true) {",
         "const now = new Date();",
         "const uri = `https://gist.githubusercontent.com/${user}/${hash}/raw?v=${now}`;",
         "const client = new HttpClient();",
-        "const response  = await  client.GetStringAsync(uri);",
+        "const response = await client.GetStringAsync(uri);",
         "const source = response.Result;",
         "return evalResult ? eval(source) : `${source}`;",
         "}"
